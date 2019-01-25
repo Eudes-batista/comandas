cdb04ee85b745fc5bf038534fb43423c15707d4b
Administrador <Administrador@PC-PC>
2019-01-25 18:00:27 -0300

    Finalizado a inclusão dos itens e a impressão fastfood

--- src/java/bean/FastFoodBean.java
+++ src/java/bean/FastFoodBean.java
@@ -3,6 +3,8 @@ package bean;
 import controle.ControlePedido;
 import java.io.IOException;
 import java.io.Serializable;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -14,6 +16,7 @@ import lombok.Getter;
 import lombok.Setter;
 import modelo.AtalhoFastFood;
 import modelo.Comandas;
+import modelo.EspelhoComanda;
 import modelo.Lancamento;
 import modelo.Lapt51;
 import modelo.Produto;
@@ -27,6 +30,7 @@ import servico.AtalhoFastFoodService;
 import servico.ComandaService;
 import servico.GrupoServico;
 import servico.ProdutoService;
+import util.GerenciaArquivo;
 
 @ManagedBean(name = "fastFoodBean")
 @ViewScoped
@@ -42,11 +46,16 @@ public class FastFoodBean implements Serializable {
     private AtalhoFastFoodService atalhoFastFoodService;
     @ManagedProperty(value = "#{controle}")
     private ComandaService controleService;
+    @ManagedProperty(value = "#{espelhoComandaBean}")
+    private EspelhoComandaBean espelhoComandaBean;    
+    @ManagedProperty(value = "#{produtoBean}")
+    private ProdutoBean produtoBean;
 
     private AtalhoFastFood atalhoFastFood;
     private Usuario usuario;
     private Comandas comandas;
     private ControlePedido controlePedido;
+    private Produto produto;
 
     private List<Lapt51> grupos;
     private List<Lancamento> lancamentos;
@@ -54,7 +63,6 @@ public class FastFoodBean implements Serializable {
 
     private String pesquisa;
     private double quantidade;
-    private Produto produto;
 
     public void init() {
         this.usuario = (Usuario) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("usuario");
@@ -64,6 +72,7 @@ public class FastFoodBean implements Serializable {
             } catch (IOException ex) {
                 Messages.addGlobalError("Erro ao redireciona para a pagina de login.");
             }
+            return;
         }
         this.atalhoFastFood = atalhoFastFoodService.carregar();
         this.grupos = grupoServico.listarGrupos();
@@ -132,9 +141,18 @@ public class FastFoodBean implements Serializable {
             return;
         }
         this.controlePedido = new ControlePedido(controleService, this.comandas.getCOMANDA());
+        List<Lancamento> lancamentosAdicionados = this.produtoBean.getLancamentosAdicionados();
         lancamentos.forEach((lancamento) -> {
             salvar(lancamento, controlePedido);
-        });
+            lancamentosAdicionados.add(lancamento);
+        });             
+        this.produtoBean.setComanda(this.comandas.getCOMANDA());
+        this.produtoBean.setMesa(this.comandas.getCOMANDA());
+        this.produtoBean.imprimirTodos();
+        this.comandas = new Comandas();
+        this.lancamentos= new ArrayList<>();
+        novoProduto();
+        PrimeFaces.current().executeScript("PF('dialogFinalizar').hide();");
     }
 
     public void novoProduto() {
@@ -147,24 +165,64 @@ public class FastFoodBean implements Serializable {
         Date data = new Date();
         sosa98.setId(sosa98Id);
         String item = this.controleService.gerarSequencia(this.comandas.getCOMANDA());
+        String numero = this.controleService.gerarNumero() + item;
+        String pedido = controlePedido.gerarNumero();
+        lancamento.setComanda(this.comandas.getCOMANDA());
+        lancamento.setMesa(this.comandas.getCOMANDA());
+        lancamento.setItem(item);
+        lancamento.setNumero(numero);
+        lancamento.setPedido(pedido);
         sosa98Id.setTenumseq(item);
-        sosa98Id.setTenumero(this.controleService.gerarNumero() + item);
+        sosa98Id.setTenumero(numero);
         sosa98.setTecdmesa(this.comandas.getCOMANDA());
         sosa98.setTecomand(this.comandas.getCOMANDA());
         sosa98.setTedatcom(data);
         sosa98.setTeimprim(lancamento.getImprimir());
-        sosa98.setTepedido(controlePedido.gerarNumero());
+        sosa98.setTepedido(pedido);
         sosa98.setTequanti(lancamento.getQuantidade());
         sosa98.setTerefere(lancamento.getReferencia());
         sosa98.setTestatus(lancamento.getStatus());
         sosa98.setTevended(lancamento.getVendedor());
         try {
             this.controleService.salvar(sosa98);
+            this.salvarEspelho(lancamento, data);
             sosa98Id = null;
             sosa98 = null;
         } catch (Exception ex) {
             Messages.addGlobalError("Erro ao salvar");
         }
+    }
+
+    private void salvarEspelho(Lancamento lancamento, Date data) {
+        EspelhoComanda espelhoComanda = new EspelhoComanda();
+        espelhoComanda.setNumero(Integer.parseInt(lancamento.getNumero()));
+        espelhoComanda.setPedido(lancamento.getPedido());
+        espelhoComanda.setMesa(lancamento.getMesa());
+        espelhoComanda.setComanda(lancamento.getComanda());
+        espelhoComanda.setData(data);
+        espelhoComanda.setNumeroItem(lancamento.getItem());
+        espelhoComanda.setReferencia(lancamento.getReferencia());
+        espelhoComanda.setPessoasMesa(this.comandas.getPESSOAS());
+        espelhoComanda.setQuantidade(lancamento.getQuantidade());
+        espelhoComanda.setVendedor(lancamento.getVendedor());
+        espelhoComanda.setImpressao(lancamento.getImprimir());
+        espelhoComanda.setStatus(lancamento.getStatus());
+        espelhoComanda.setObservacao(lancamento.getObservacao());
+        espelhoComanda.setStatusItem("N");
+        espelhoComanda.setValorItem(lancamento.getPreco());
+        try {
+            String dataPreconta = espelhoComandaBean.buscarDataPreconta(lancamento.getPedido());
+            Date dataPrecontaBanco = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(dataPreconta);
+            espelhoComanda.setDataPreconta(dataPrecontaBanco);
+        } catch (ParseException ex) {
+        }
+        if (!new GerenciaArquivo().bucarInformacoes().getConfiguracao().getCobraDezPorcento().isEmpty()) {
+            espelhoComanda.setPorcentagem(10d);
+            double valorComDezPOrcento = lancamento.getPrecoTotal() * 0.10;
+            espelhoComanda.setValorPorcentagem(valorComDezPOrcento);
+        }
+        espelhoComandaBean.setEspelhoComanda(espelhoComanda);
+        espelhoComandaBean.salvar();
     }
 
 }
