c10832872a70bcca9151740bacaa325ca5cb5bf1
Administrador <Administrador@PC-PC>
2019-02-26 17:18:45 -0300

    Adicionado tela de perfil de usuario

--- src/java/util/HibernateUtil.java
+++ src/java/util/HibernateUtil.java
@@ -6,14 +6,14 @@ import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
 
-    private static SessionFactory sessionFactory=null;
-    
+    private static SessionFactory sessionFactory = null;
+
     public static SessionFactory getSessionFactory() {
         try {
             Configuration configuration = new Configuration();
             configuration.configure("hibernate.cfg.xml");
-            if(sessionFactory==null){
-                sessionFactory= configuration.buildSessionFactory();
+            if (sessionFactory == null) {
+                sessionFactory = configuration.buildSessionFactory();
             }
         } catch (HibernateException ex) {
             System.err.println("Initial SessionFactory creation failed." + ex);
