fc0185570450c1d864a5762e71c29523ce04c899
Administrador <Administrador@PC-PC>
2019-10-15 14:49:34 -0300

    Criando procedure de inserção de comandas e criação de trigger para chamar a procedure

--- src/java/util/ValoresIniciais.java
+++ src/java/util/ValoresIniciais.java
@@ -9,11 +9,15 @@ public class ValoresIniciais {
         this.criarGeradorDeSeguenciaCancelamentoMesa();
         this.alterarCamposEspelhoComandaSosa98();
         this.incluirValoresIncias();
+        this.apagarProcedure();
+        this.criarProcedureInserirComandas();
+        this.apagarTriggerExcluirComandaSosa98();
+        this.criarTriggerExcluirComandaSosa98();
     }
 
     private void incluirValoresIncias() {
-        executarSql("insert into motivo_cancelamento (codigo,nome) values(99,'EXCLUSAO DE MESA')");
-        executarSql("insert into motivo_cancelamento (codigo,nome) values(100,'EXCLUSAO DE COMANDA')");
+        executarSql("update or insert into motivo_cancelamento (codigo,nome) values(99,'EXCLUSAO DE MESA')");
+        executarSql("update or insert into motivo_cancelamento (codigo,nome) values(100,'EXCLUSAO DE COMANDA')");
     }
 
     private void criarGeradorDeSeguenciaCancelamentoMesa() {
@@ -25,6 +29,37 @@ public class ValoresIniciais {
         executarSql("alter table espelho_comanda alter pedido type varchar(9)");
         executarSql("alter table sosa98 alter tepedido type varchar(9)");
         executarSql("alter table item_acompanhamento alter pedido type varchar(9)");
+    }
+
+    private void criarProcedureInserirComandas() {
+        String sql = "create procedure INSERIR_COMANDAS (comanda varchar(4)) as\n"
+                + "  DECLARE VARIABLE quantidade INTEGER; \n"
+                + "begin    \n"
+                + "  select count(TECOMAND) as qtd from sosa98 where TECOMAND = :comanda INTO quantidade;  \n"
+                + "  IF (quantidade = 1) THEN\n"
+                + "    BEGIN\n"
+                + "      insert into COMANDAS_EXCLUIDAS (COMANDA) select TECOMAND from sosa98 where TECOMAND=:comanda group by TECOMAND;\n"
+                + "    END\n"
+                + "END";
+        this.executarSql(sql);
+    }
+
+    private void apagarProcedure() {
+        String sql = "DROP PROCEDURE INSERIR_COMANDAS;";
+        this.executarSql(sql);
+    }
+
+    private void apagarTriggerExcluirComandaSosa98() {
+        String sql = "DROP TRIGGER ANTES_DELETAR;";
+        this.executarSql(sql);
+    }
+
+    private void criarTriggerExcluirComandaSosa98() {
+        String sql = " CREATE TRIGGER ANTES_DELETAR FOR SOSA98 BEFORE DELETE POSITION 0 AS \n"
+                + "BEGIN \n"
+                + "  execute procedure INSERIR_COMANDAS OLD.TECOMAND; \n"
+                + "END ";
+        this.executarSql(sql);
     }
 
     private void executarSql(String sql) {
