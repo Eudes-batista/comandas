a8fff17f0d4b91135587f7b3d49ddb20ea261737
Administrador <Administrador@PC-PC>
2019-10-16 09:17:32 -0300

    Tabela que irá registrar as comandas excluidas

--- src/java/modelo/ComandaExcluida.java
+++ src/java/modelo/ComandaExcluida.java
@@ -1,7 +1,25 @@
-
 package modelo;
 
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
-public class ComandaExcluida {
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@Entity
+@Table(name = "COMANDAS_EXCLUIDAS")
+public class ComandaExcluida implements Serializable {
+
+    @Id
+    @Column(name = "comanda", length = 4, nullable = false)
+    private String comanda;
 
 }
